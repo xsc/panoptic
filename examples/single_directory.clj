53dbf25cc512dc3ef944afb9c1e9f3f970a03dd5
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-14 12:33:59 +0200

    Added Directory Watcher Example.

---
+++ examples/single_directory.clj
@@ -0,0 +1,15 @@
+(ns single-directory
+  (:use panoptic.core))
+
+(defn -main
+  [& [path & _]]
+  (if path
+    (do
+      (println "Watching" path "...")
+      (let [w (-> (simple-directory-watcher path :interval 200)
+                (on-directory-file-create #(println "   File" %3 "created."))
+                (on-directory-file-delete #(println "   File" %3 "deleted."))
+                (on-directory-create #(println "   Directory" %3 "created."))
+                (on-directory-delete #(println "   Directory" %3 "deleted.")))]
+        @(start-watcher! w))) 
+    (println "Usage: lein run-example single-directory <path to directory>")))
