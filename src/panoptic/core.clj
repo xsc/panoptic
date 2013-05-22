615fbd3ad25999d29aa42c82d7ddc03be17dc2a8
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-23 01:33:48 +0200

    Offering access to log level in panoptic.core

--- src/panoptic/core.clj
+++ src/panoptic/core.clj
@@ -2,27 +2,10 @@
       :author "Yannick Scherer"}
   panoptic.core
   (:use [potemkin :only [import-vars]])
-  (:require [panoptic.watchers core file-watcher directory-watcher]
+  (:require [taoensso.timbre :as timbre :only [set-level]]
+            [panoptic.watchers core file-watcher directory-watcher]
             [panoptic.runners simple multi-threaded]
             [panoptic.data file directory]))
-
-;; ## Concept
-;;
-;; There are different "file watchers" that read the files they have to check
-;; from an atom, writing updated values back to it. The atom has a common watcher
-;; attached that will call corresponding handlers for file creation/deletion/modification.
-;;
-;; There are "directory watchers" that read the directories they have to check from
-;; an atom, writing updated values back to it. New files will then be distributed
-;; to the existing "file watchers" (whilst deleted files will automatically disappear 
-;; from the system, since the file watchers will invalidate them.
-;;
-;; The file watchers have different polling intervals. Files that have been freshly changed are
-;; polled more often than those that have not been changed for a long time. However, 
-;; files untouched for a longer span of time are distributed randomly to watchers with capacity
-;; to anticipate potential changes and detect them quickly.
-;;
-;; The initial interval given is the maximum time a file can go without being polled.
 
 ;; ## Import
 
@@ -67,3 +50,12 @@
 
    on-directory-create
    on-directory-delete])
+
+;; ## Logging Access
+
+(timbre/set-level! :warn)
+
+(defn set-log-level!
+  "Set Panoptic/Timbre Log Level."
+  [l]
+  (timbre/set-level! l))
