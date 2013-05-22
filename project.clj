ce812eb6129f8bdaffcda528476c866d9a41f58f
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-22 16:19:45 +0200

    Upgraded to pandect 0.2.2

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [potemkin "0.2.2"]
-                 [pandect "0.2.1"]]
+                 [pandect "0.2.2"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [log4j "1.2.17"]]  
                    :plugins [[lein-midje "3.0.1"]]}
