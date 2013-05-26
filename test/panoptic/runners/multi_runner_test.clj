9be479019be65d3bc25d008164e23608863fde2e
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-27 01:27:54 +0200

    Tests for Runners.

---
+++ test/panoptic/runners/multi_runner_test.clj
@@ -0,0 +1,56 @@
+(ns ^{:doc "Tests for Multi Runners"
+      :author "Yannick Scherer"}
+  panoptic.runners.multi-runner-test
+  (:require [taoensso.timbre :as timbre :only [set-level!]])
+  (:use midje.sweet
+        panoptic.watchers.core
+        panoptic.runners.core
+        panoptic.runners.multi-runner
+        panoptic.utils.core))
+
+(timbre/set-level! :warn)
+
+(tabular
+  (tabular
+    (tabular
+      (let [a (atom 0)
+            f (watch-fn #(assoc % :v @a) nil (constantly {})) 
+            w (multi-runner f ?distribute ?threads ?u)
+            reset-a! #(do (reset! a %) (sleep (* 2 ?u)))
+            es (take (* 2 ?threads) (map keyword (repeatedly gensym)))]
+        (fact "about multi-runner"
+          w => #(satisfies? WatchRunner %)
+          (watched-entities w) => {}
+
+          (watch-entities! w es) => truthy
+          (let [ws (watched-entities w)]
+            (count ws) => (count es)
+            (vals ws) => #(every? empty? %))
+
+          (start-watcher! w)
+
+          (reset-a! 0)
+          (let [ws (watched-entities w)]
+            (count ws) => (count es)
+            (map :v (vals ws)) => #(every? zero? %))
+
+          (reset-a! 1)
+          (let [ws (watched-entities w)]
+            (count ws) => (count es)
+            (map :v (vals ws)) => #(every? (fn [x] (= x 1)) %))
+
+          (sleep (* ?threads ?u))
+          (reset-a! 2)
+          (let [ws (watched-entities w)]
+            (count ws) => (count es)
+            (map :v (vals ws)) => #(every? (fn [x] (= x 2)) %))
+
+          (unwatch-entities! w es) => truthy
+          (watched-entities w) => {}
+
+          (let [stop (stop-watcher! w)]
+            stop => future?
+            @stop => falsey)))
+      ?distribute :simple :fair :frequency)
+    ?threads 1 2 4 8)
+  ?u 10)
---
+++ test/panoptic/runners/simple_runner_test.clj
@@ -0,0 +1,33 @@
+(ns ^{:doc "Tests for Simple Runners"
+      :author "Yannick Scherer"}
+  panoptic.runners.simple-runner-test
+  (:require [taoensso.timbre :as timbre :only [set-level!]])
+  (:use midje.sweet
+        panoptic.watchers.core
+        panoptic.runners.core
+        panoptic.runners.simple-runner
+        panoptic.utils.core))
+
+(timbre/set-level! :warn)
+          
+(tabular
+  (let [a (atom 0)
+        f (watch-fn (fn [_] @a) nil (constantly 0))
+        reset-a! #(do (reset! a %) (sleep (* 2 ?u)))]
+    (fact "about simple runners"
+      (let [w (simple-runner f ?u)]
+        (reset-a! 0)
+        (watch-entities! w [:a :b]) => truthy
+        (watched-entities w) => { :a 0 :b 0 }
+        (start-watcher! w)
+        (reset-a! 1)
+        (watched-entities w) => { :a 1 :b 1 }
+        (watch-entity! w :c) => truthy
+        (reset-a! 2)
+        (watched-entities w) => { :a 2 :b 2 :c 2 }
+        (unwatch-entities! w [:a :b :c]) => truthy
+        (watched-entities w) => {}
+        (let [stop (stop-watcher! w)]
+          stop => future?
+          @stop => falsey)))) 
+    ?u 10)
