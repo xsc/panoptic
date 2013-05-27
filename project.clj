a82a74312857f3e57c73d83f18e44442d1bee345
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-27 16:25:40 +0200

    Not 'dev' profile has its Clojure back.

--- project.clj
+++ project.clj
@@ -3,17 +3,18 @@
   :url "https://github.com/xsc/panoptic"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.taoensso/timbre "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/timbre "1.6.0"]
                  [potemkin "0.2.2"]
                  [pandect "0.2.3"]]
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :exclusions [org.clojure/clojure]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.1"]]}
+  :profiles {:test {:dependencies [[midje "1.5.1"]]
+                    :plugins [[lein-midje "3.0.1"]]}
              :1.3  {:dependencies  [[org.clojure/clojure "1.3.0"]]}
              :1.4  {:dependencies  [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies  [[org.clojure/clojure "1.5.1"]]}
              :1.6  {:dependencies  [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :examples {:source-paths ["examples"]}}
-  :aliases {"run-example" ["with-profile" "1.5,dev,examples" "run" "-m"]
-            "all" ["with-profile" "1.3,dev:1.4,dev:1.5,dev:1.6,dev"]})
+  :aliases {"run-example" ["with-profile" "dev,examples" "run" "-m"]
+            "midje-all" ["with-profile" "1.3,test:1.4,test:1.5,test:1.6,test" "do" "clean," "midje"]})
