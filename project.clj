1312f6bac1235f67de6a3c439f29204187ac593f
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-27 17:05:53 +0200

    Release 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject panoptic "0.2.1-SNAPSHOT"
+(defproject panoptic "0.2.1"
   :description "File & Directory Monitoring for Clojure."
   :url "https://github.com/xsc/panoptic"
   :license {:name "Eclipse Public License"
